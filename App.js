15b7a27f52c0eabb73e43b0ae794356db6a618a5
sohnryang <ryang030528@gmail.com>
2017-07-12 21:33:08 +0900

    temporarily changed App component to render nothing

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     render() {
         return (
-            <h1>Hello, world!</h1>
+            <div></div>
         );
     }
 }
