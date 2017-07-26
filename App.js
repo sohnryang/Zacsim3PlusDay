e0e83357061b0883b800869e3eea0236d019ea07
sohnryang <ryang030528@gmail.com>
2017-07-26 22:58:47 +0900

    temporarily added habit hi

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Habit from './Habit';
 
 class App extends React.Component {
     render() {
         return (
-            <div></div>
+            <div><Habit name="hi" /></div>
         );
     }
 }
